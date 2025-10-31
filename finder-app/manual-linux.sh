d68baf581ee272f97bbf094b03e9b076535a16c6
Stephen Tellis (DC-MH/EME4) <stephen.tellis@boschrexroth.de>
2025-10-31 14:54:08 +0100

    Assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -6,12 +6,16 @@ set -e
 set -u
 
 OUTDIR=/tmp/aeld
-KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+
+export PATH=/home/ttl5kor/LSP/toolchain_install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin:$PATH
+SYSROOT=/home/ttl5kor/LSP/toolchain_install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu
+COMMON="ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}"
 
 if [ $# -lt 1 ]
 then
@@ -34,10 +38,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make $COMMON mrproper
+    make $COMMON defconfig
+    make -j4 $COMMON all
+    make $COMMON modules
+    make $COMMON dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,34 +56,65 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+cd "$OUTDIR"
+mkdir -p rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+mkdir -p home/conf
 
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
+if [ ! -d "${OUTDIR}/busybox" ]; then
+git clone https://github.com/mirror/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+make $COMMON
+make CONFIG_PREFIX=${OUTDIR}/rootfs $COMMON install # This will create the symlinks for us
 
 echo "Library dependencies"
+cd "$OUTDIR/rootfs"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+cp "${SYSROOT}/libc/lib/ld-linux-aarch64.so.1" "$OUTDIR/rootfs/lib/"
+cp "${SYSROOT}/libc/lib64/libm.so.6" "$OUTDIR/rootfs/lib64/libm.so.6"
+cp "${SYSROOT}/libc/lib64/libc.so.6" "$OUTDIR/rootfs/lib64/libc.so.6"
+cp "${SYSROOT}/libc/lib64/libresolv.so.2" "$OUTDIR/rootfs/lib64/libresolv.so.2"
 
-# TODO: Make device nodes
+# Make device nodes
+cd "$OUTDIR/rootfs"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 622 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -a "$FINDER_APP_DIR"/writer "$OUTDIR"/rootfs/home/writer
+cp -a "$FINDER_APP_DIR"/finder.sh "$OUTDIR"/rootfs/home/finder.sh
+cp -a "$FINDER_APP_DIR"/finder-test.sh "$OUTDIR"/rootfs/home/finder-test.sh
+cp -a "$FINDER_APP_DIR"/conf/username.txt "$OUTDIR"/rootfs/home/conf/username.txt
+cp -a "$FINDER_APP_DIR"/conf/assignment.txt "$OUTDIR"/rootfs/home/conf/assignment.txt
+cp -a "$FINDER_APP_DIR"/autorun-qemu.sh "$OUTDIR"/rootfs/home/autorun-qemu.sh
+chmod +x "$OUTDIR"/rootfs/home/finder.sh
 
-# TODO: Chown the root directory
+# Chown the root directory
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > "$OUTDIR"/initramfs.cpio
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+cd "$OUTDIR"
+gzip -f initramfs.cpio
